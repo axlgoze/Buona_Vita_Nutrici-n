a6537ce66b3c111d887287b30fd56cd00445c738
Axel Reyes <rbaxel.f@gmail.com>
2022-07-22 16:40:06 -0500

    Agregando código inicial

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// querySelector permite acceder al mundo html
+var titulo = document.querySelector(".titulo");
+
+console.log(titulo) // muestra el elmento
+
+console.log(titulo.textContent) // muestra el contenido del elemento
+
+// js nos permite modificar el DOM del html
+titulo.textContent= "Buena vida Nutrición" // modifica el contenido de texto de  la clase titulo
+
+// indice de masa corporal
+var paciente = querySelector("#");
