8e6db3160eecb64a8a47471d96b62bfd47f7c9a2
Axel Reyes <rbaxel.f@gmail.com>
2022-07-22 17:15:47 -0500

    obteniendo valores a partir del objeto PACIENTE

--- js/main.js
+++ js/main.js
@@ -8,30 +8,29 @@ console.log(titulo.textContent) // muestra el contenido del elemento
 // js nos permite modificar el DOM del html
 titulo.textContent= "Buena vida Nutrición" // modifica el contenido de texto de  la clase titulo
 
-// indice de masa corporal
+// calculando indice de masa corporal
 var paciente = document.querySelector("#primerPaciente");
 
 console.log(paciente.textContent);
 // console.log(paciente);
 
-// obtener peso
 
-var tdpeso = document.querySelector(".info-peso");
+// ***************************************************OBTENER PESO
 
-var peso = tdpeso.textContent;
+// var tdpeso = document.querySelector(".info-peso");
+// var peso = tdpeso.textContent;
 
-console.log(peso)
 
+// accediento a la informacion a partir del objeto-paciente y la clase "info-peso"
+var peso = paciente.querySelector(".info-peso");
+console.log(peso);
 
 // obtener altura
 
-var tdaltura = document.querySelector(".info-altura")
-
-var altura = tdaltura.textContent;
-
+var altura = paciente.querySelector(".info-altura")
 console.log(altura)
 
-// imc = peso * altura * altura
+// imc = peso / altura * altura
 
-imc = peso*altura*altura;
-console.log(imc)
+imc = peso/(altura*altura);
+console.log(imc);
