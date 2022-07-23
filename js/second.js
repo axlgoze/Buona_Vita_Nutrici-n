815e2eeafa29a8557bb1d3f779a42fd7ffffb2b1
Axel Reyes <rbaxel.f@gmail.com>
2022-07-23 16:12:44 -0500

    add: ciclo for para recorrer cada paciente

--- js/second.js
+++ js/second.js
@@ -5,4 +5,47 @@ var pacientes = document.querySelectorAll(".paciente"); // querySelectorAll perm
 
 console.log(pacientes);
 
+for(var i=0;i<pacientes.length;i++){
+    // accede a cada elemento del arreglo
+    var paciente=pacientes[i];
 
+    var peso = paciente.querySelector(".info-peso");
+    console.log(peso);
+
+    var altura = paciente.querySelector(".info-altura")
+    console.log(altura)
+
+    var tdIMC = paciente.querySelector(".info-imc");
+
+    var Peso=parseInt(peso.textContent);
+    var Altura=parseInt(altura.textContent);
+
+    var imc =  Peso/(Altura*Altura) ;
+
+    // condicionales
+
+    pesoValido = true;
+    alturaValida = true;
+
+    var error="Error";
+
+    if(Peso < 0 || Peso > 1000){
+        peso.textContent=error + ": Peso Incorrecto";
+        pesoValido = false;
+        tdIMC.textContent = error;
+        console.log(imc);
+    }
+
+    if(Altura < 0 || Altura > 3.00){
+        altura.textContent=error + ": Altura Incorrecta";
+        alturaValida=false;
+        tdIMC.textContent = error;
+        console.log(imc);
+    }
+
+
+    if(pesoValido && alturaValida){
+        // asignando nuevo valor
+        tdIMC.textContent = imc;
+    }
+}
