f29fc0df2275a5e230e1bed375533b9a5c39f24b
Axel Reyes <rbaxel.f@gmail.com>
2022-07-26 13:41:31 -0500

    delegando responsabilidades: captura de datos

--- js/form.js
+++ js/form.js
@@ -6,11 +6,8 @@ botonAdicionar.addEventListener("click",function(event){
     event.preventDefault();
 
     var form = document.querySelector("#form-adicional");
-    //captura de valores accediendo a los objetos de html
-    var nombre = form.nombre.value;
-    var peso = form.peso.value;
-    var  altura = form.altura.value;
-    var porcentaje = form.gordura.value;
+
+    var paciente = capturaDatos(form);
     
     //row creation
     var pacienteTr = document.createElement("tr");
@@ -22,7 +19,7 @@ botonAdicionar.addEventListener("click",function(event){
     gorduraTd = document.createElement("td");
 
     imcTd =document.createElement("td");
-
+    //exhibiendo informacion con propiedad textContent
     nombreTd.textContent = nombre;
     pesoTd.textContent = peso;
     alturaTd.textContent = altura;
@@ -30,7 +27,7 @@ botonAdicionar.addEventListener("click",function(event){
 
     imcTd.textContent = calcularIMC(peso,altura);
 
-    //conectar js con html
+    //asignar nodos a table row created
     pacienteTr.appendChild(nombreTd);
     pacienteTr.appendChild(pesoTd);
     pacienteTr.appendChild(alturaTd);
@@ -44,3 +41,16 @@ botonAdicionar.addEventListener("click",function(event){
     console.log(pacienteTr);
 
 });
+
+
+function capturaDatos(form){
+    //captura de valores accediendo a los objetos de html
+
+    
+    var nombre = form.nombre.value;
+    var peso = form.peso.value;
+    var  altura = form.altura.value;
+    var porcentaje = form.gordura.value;
+
+    return paciente.push
+}
