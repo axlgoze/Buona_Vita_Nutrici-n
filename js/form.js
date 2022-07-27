0150c271e0fb2ee01e354976d2f01424b9ebe96a
Axel Reyes <rbaxel.f@gmail.com>
2022-07-27 18:10:02 -0500

    Encapsulando funcion para crear elementos y asignarlos como nodos

--- js/form.js
+++ js/form.js
@@ -2,50 +2,21 @@ var botonAdicionar = document.querySelector("#adicionar-paciente");
 
 // uso de funcion anonima
 botonAdicionar.addEventListener("click",function(event){
-    // permite no recargar la pagin una vez realizado el evento
+    // permite no recargar la pagina una vez realizado el evento
     event.preventDefault();
 
     var form = document.querySelector("#form-adicional");
-
     var paciente = capturaDatos(form);
-    console.log(paciente)
-    //row creation
-    var pacienteTr = document.createElement("tr");
-
-    //td creation
-    nombreTd = document.createElement("td");
-    pesoTd = document.createElement("td");
-    alturaTd = document.createElement("td");
-    gorduraTd = document.createElement("td");
-
-    imcTd =document.createElement("td");
-    //exhibiendo informacion con propiedad textContent
-    nombreTd.textContent = paciente.nombre;
-    pesoTd.textContent = paciente.peso;
-    alturaTd.textContent = paciente.altura;
-    gorduraTd.textContent = paciente.porcentaje;
-
-    imcTd.textContent = paciente.imc
-
-    //asignar nodos a table row created
-    pacienteTr.appendChild(nombreTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
-
+    var pacienteTr = createTr(paciente);
     var tabla = document.querySelector("#tabla-pacientes");
-
     tabla.appendChild(pacienteTr);
 
-    console.log(pacienteTr);
+    
 
 });
 
 
 function capturaDatos(form){
-    //captura de valores accediendo a los objetos de html
-
     //creacion de objeto para poder retornarlo
     var paciente = {
         nombre : form.nombre.value,
@@ -56,4 +27,29 @@ function capturaDatos(form){
     }
 
     return paciente;
+}
+
+function createTr(paciente) {
+    //row creation
+    var pacienteTr = document.createElement("tr");
+    //td creation
+    var nombreTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd =document.createElement("td");
+    //exhibiendo informacion con propiedad textContent
+    nombreTd.textContent = paciente.nombre;
+    pesoTd.textContent = paciente.peso;
+    alturaTd.textContent = paciente.altura;
+    gorduraTd.textContent = paciente.porcentaje;
+    imcTd.textContent = paciente.imc
+    //asignar nodos a table row created
+    pacienteTr.appendChild(nombreTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    return pacienteTr
 }
