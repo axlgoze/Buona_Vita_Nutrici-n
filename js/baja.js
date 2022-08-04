04570c645c83f90cebebe11106b9d2d8ec180f4c
Axel Reyes <rbaxel.f@gmail.com>
2022-08-04 14:22:40 -0500

    Haciendo uso de target + parentNode + remove

--- js/baja.js
+++ js/baja.js
@@ -1,9 +1,13 @@
 var pacientes = document.querySelectorAll(".paciente"); // seleciona pacientes nativos
-console.log(pacientes);
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick",function(){
-        // console.log("Doble click realizado");
-        this.remove();
-    });
+// pacientes.forEach(function(paciente){
+//     paciente.addEventListener("dblclick",function(){
+//         // console.log("Doble click realizado");
+//         this.remove();
+//     });
+// });
+
+var tabla = document.querySelector("#tabla-pacientes");
+tabla.addEventListener("dblclick",function(event){
+    event.target.parentNode.remove();
 });
