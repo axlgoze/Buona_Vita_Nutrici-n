169846ca2247f9cb98b1465ba073fd053485300d
Axel Reyes <rbaxel.f@gmail.com>
2022-08-06 14:46:10 -0500

    añadiendo logica para buscar pacientes y esconder lo no buscados

--- js/filtrar.js
+++ js/filtrar.js
@@ -3,15 +3,29 @@ var campoFiltro = document.querySelector("#filtrar-tabla");
 
 campoFiltro.addEventListener("input",function(){
     // console.log("Entrada detectada");
-    console.log(this.value);
-
+    // console.log(this.value);
     var pacientes = document.querySelectorAll(".paciente");
 
-    // accediendo al nombre de los pacientes
-    for(var i=0; i<pacientes.length;i++){
-        var paciente = pacientes[i];
-        var tdName = paciente.querySelector(".info-nombre");
-        var nombre = tdName.textContent;
-        console.log(nombre);
+    if(this.value.length > 0){
+        console.log("there's something typed");
+        // accediendo al nombre de los pacientes
+        for(var i=0; i<pacientes.length;i++){
+            var paciente = pacientes[i];
+            var tdName = paciente.querySelector(".info-nombre");
+            var nombre = tdName.textContent;
+            
+            if(nombre != this.value){
+                paciente.classList.add("invisible");
+            }else{
+                paciente.classList.remove("invisible");
+            }
+        }
+    }else{
+        for(var i = 0; i< pacientes.length; i++){
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisible");
+        }
     }
+    
+
 });
